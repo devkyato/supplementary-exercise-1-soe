050ce3a13a6d34140e3c0ff4ace9e19a441a66fb
@dev.mako <95612413+devkyato@users.noreply.github.com>
2026-03-06 22:18:28 +0800

    Update Exercise_B.ino

--- Exercise_B.ino
+++ Exercise_B.ino
@@ -1,39 +1,41 @@
-int led_1 = 6;
-int led_2 = 7;
-int led_3 = 8;
- 
- 
+// tatlong leds para sa traffic light
+int led_1 = 6; // green
+int led_2 = 7; // yellow
+int led_3 = 8; // red
+
+// delay values para sa bawat ilaw
 int led_delay1 = 5000;
 int led_delay2 = 2000;
 int led_delay3 = 5000;
- 
+
 void setup() {
+
+  // set pins as output
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
- 
- 
-  // put your setup code here, to run once:
- 
 }
- 
+
 void loop() {
- 
-  digitalWrite(led_1, HIGH); //
+
+  // green light
+  digitalWrite(led_1, HIGH);
   digitalWrite(led_2, LOW);
-  digitalWrite(led_3, LOW); //
- 
-  delay(5000);
- 
-  digitalWrite(led_1, LOW);
-  digitalWrite(led_2, HIGH); //
   digitalWrite(led_3, LOW);
- 
-  delay(2000);
- 
+
+  delay(5000); // 5 seconds green
+
+  // yellow light
+  digitalWrite(led_1, LOW);
+  digitalWrite(led_2, HIGH);
+  digitalWrite(led_3, LOW);
+
+  delay(2000); // 2 seconds yellow
+
+  // red light
   digitalWrite(led_1, LOW);
   digitalWrite(led_2, LOW);
   digitalWrite(led_3, HIGH);
- 
-  delay(5000);
+
+  delay(5000); // 5 seconds red
 }
